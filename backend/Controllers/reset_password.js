81c164a97edc5b9f192d1e35eb1e407f2aa0dfa1
hager <hagermahmod@gmail.com>
2026-04-07 16:44:19 +0200

    reset password update

--- backend/Controllers/reset_password.js
+++ backend/Controllers/reset_password.js
@@ -1,21 +1,24 @@
     import express from 'express';
     import { hash } from 'bcrypt';
-
+    import { Users } from "../Models/user.model.js";
 
 
     const resetPassword = async (req, res) => {
         try {
-            const { email, otp, password } = req.body;
+            const { email, otp, password, confirmPassword } = req.body;
 
-            if (!email || !otp || !password) {
+            if (!email || !otp || !password || !confirmPassword) {
                 return res.status(400).json({ message: 'All fields are required' });
             }
 
-            const user = await User.findOne({ email });
+            const user = await Users.findOne({ email });
             if (!user) {
                 return res.status(400).json({ message: 'User not found' });
             }
 
+            if (password !== confirmPassword) {
+                return res.status(400).json({ message: 'Passwords do not match' });
+            }
 
             if (user.otp !== otp) {
                 return res.status(400).json({ message: 'Invalid OTP' });
