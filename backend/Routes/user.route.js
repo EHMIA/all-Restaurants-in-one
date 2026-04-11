ecc1869fd3ca7c6b2fb92daea60715f12f92e412
hager <hagermahmod@gmail.com>
2026-04-11 06:07:26 +0200

    ------

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -3,12 +3,13 @@ const router=Router();
 import { getUserProfile } from "../Controllers/GetUserProfile.js";
 import { uploadPhoto } from "../Controllers/UploadPhoto.js";
 import { Protect } from "../Middlewares/auth.middleware.js";
+import { restrictToAdminOrAccountOwner } from "../Middlewares/auth.middleware.js";
 import { uploadSingleImage } from "../Middlewares/upload.middleware.js";
 import { editUserProfile } from "../Controllers/EditUser.js";
 
 router.get('/getUserProfile', Protect, getUserProfile);
 router.post('/uploadPhoto', Protect, uploadSingleImage, uploadPhoto);
-router.post('/editUserProfile/:id', Protect, editUserProfile);
+router.post('/editUserProfile/:id', Protect, restrictToAdminOrAccountOwner, editUserProfile);
 
 
 
