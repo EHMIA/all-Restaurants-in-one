6c7e3823c192c16bbf03b0da46afeb24cbf5224d
hager <hagermahmod@gmail.com>
2026-05-07 19:41:00 +0300

    get all users

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -2,13 +2,13 @@ import {Router} from "express"
 const router=Router();
 import { getUserProfile , uploadProfilePicController , editUserProfile , deleteAccountController , deleteProfilePicController , changePasswordController , getAllUsers} from "../Controllers/user.controller.js";
 import { Protect } from "../Middlewares/auth.middleware.js";
-import { restrictToAdminOrAccountOwner } from "../Middlewares/auth.middleware.js";
+import { restrictToAdminOrAccountOwner , restrictToAdmin } from "../Middlewares/auth.middleware.js";
 import { uploadSingleImage } from "../Middlewares/upload.middleware.js";
 import { getMyRestaurantDashboard } from "../Controllers/restaurant.controller.js";
 
 
 router.get('/getUserProfile', Protect, restrictToAdminOrAccountOwner, getUserProfile);
-router.get('/getAllUsers', restrictToAdminOrAccountOwner, getAllUsers);
+router.get('/getAllUsers',Protect, restrictToAdmin, getAllUsers);
 router.patch('/editUserProfile/:id', Protect, restrictToAdminOrAccountOwner, editUserProfile);
 router.patch('/uploadProfilePhoto', Protect, uploadSingleImage, uploadProfilePicController);
 router.delete('/deleteProfilePhoto/:id', Protect, restrictToAdminOrAccountOwner, deleteProfilePicController);
