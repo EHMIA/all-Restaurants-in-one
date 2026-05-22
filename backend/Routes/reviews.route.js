64ec0de9667fde3df6364d23841e28cca0f8181f
hager <hagermahmod@gmail.com>
2026-05-22 16:39:10 +0300

    getOneReview

--- backend/Routes/reviews.route.js
+++ backend/Routes/reviews.route.js
@@ -1,11 +1,13 @@
 import {Router} from "express"
-import { addReview, deleteReview, getMyReviews } from "../Controllers/reviews.controller.js";
+import { addReview, deleteReview, getMyReviews , getOneReview} from "../Controllers/reviews.controller.js";
 import { Protect, restrictToAccountOwner } from "../Middlewares/auth.middleware.js";
 
 const router= Router();
 
 
 router.get("/my-reviews",Protect,restrictToAccountOwner ,getMyReviews);
+
+router.get("/my-reviews/:restaurantId",Protect,restrictToAccountOwner ,getOneReview);
 
 router.post("/add-review/:restaurantId",Protect,restrictToAccountOwner, addReview);
 
