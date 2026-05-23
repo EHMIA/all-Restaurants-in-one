4e5e47abf70926cb0d8d1ed42d25ae9098306d2d
safaa abdelkhalek <safaa3bdelkhaleq@gmail.com>
2026-05-23 11:51:18 +0300

    -

--- backend/Controllers/auth.controller.js
+++ backend/Controllers/auth.controller.js
@@ -94,8 +94,8 @@ const login = async (req, res) => {
             id: user._id,
             fullname: user.fullname,
                 profile_pic: {
-                    url: user.profile_pic.url,
-                    publicId: user.profile_pic.publicId
+                    url: user.profile_pic?.url || null, 
+                    publicId: user.profile_pic?.publicId || null
                 },
             email: user.email,
             phone: user.phone
